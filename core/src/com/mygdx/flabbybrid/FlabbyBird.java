919a7a88783d906bb961437ef62d670ddeaabd1b
harper0201 <963444707@qq.com>
2021-11-12 09:57:06 -0500

    fifth commit: collision detection

--- core/src/com/mygdx/flabbybrid/FlabbyBird.java
+++ core/src/com/mygdx/flabbybrid/FlabbyBird.java
@@ -3,8 +3,13 @@ package com.mygdx.flabbybrid;
 import com.badlogic.gdx.AbstractGraphics;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.Circle;
+import com.badlogic.gdx.math.Intersector;
+import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 import java.util.Random;
@@ -28,6 +33,11 @@ public class FlabbyBird extends ApplicationAdapter {
 	float []tubeX = new float[numberOfTubes];
 	float []tubeOffset = new float[numberOfTubes];
 	float distanceBetweenTubes;
+	Circle birdCircle;
+	//ShapeRenderer shapeRenderer;
+	Rectangle[] topTubeRectangles;
+	Rectangle[] buttonTubeRectangles;
+
 
 
 	@Override
@@ -35,6 +45,8 @@ public class FlabbyBird extends ApplicationAdapter {
 		batch = new SpriteBatch();
 		background = new Texture("bg.png");
 		birds = new Texture[2];
+		birdCircle = new Circle();
+		//shapeRenderer = new ShapeRenderer();
 		birds[0] = new Texture("bird.png");
 		birds[1] = new Texture("bird2.png");
 		birdY  = Gdx.graphics.getHeight()/2 - birds[0].getWidth()/2;
@@ -43,10 +55,15 @@ public class FlabbyBird extends ApplicationAdapter {
 		maxTubeOffset = Gdx.graphics.getHeight()/2 - gap/2 - 100;
 		ramdomGenerotor = new Random();
 		distanceBetweenTubes = Gdx.graphics.getWidth()*3/4;
+		topTubeRectangles = new Rectangle[numberOfTubes];
+		buttonTubeRectangles = new Rectangle[numberOfTubes];
 		for(int i = 0; i < numberOfTubes; i++){
-			tubeX[i] = Gdx.graphics.getWidth()/2 - topTube.getWidth() + i*distanceBetweenTubes;
 			tubeOffset[i] =
 					(ramdomGenerotor.nextFloat() - 0.5f) *(Gdx.graphics.getHeight()- gap - 200);
+			tubeX[i] =
+					Gdx.graphics.getWidth()/2 - topTube.getWidth() + Gdx.graphics.getWidth() + i*distanceBetweenTubes;
+			topTubeRectangles[i] = new Rectangle();
+			buttonTubeRectangles[i] = new Rectangle();
 
 		}
 	}
@@ -63,6 +80,8 @@ public class FlabbyBird extends ApplicationAdapter {
 			for(int i = 0 ; i < numberOfTubes; i++){
 				if(tubeX[i] < -topTube.getWidth()){
 					tubeX[i] = numberOfTubes * distanceBetweenTubes;
+					tubeOffset[i] =
+							(ramdomGenerotor.nextFloat() - 0.5f) *(Gdx.graphics.getHeight()- gap - 200);
 				}
 				else{
 					tubeX[i] = tubeX[i] - tubeVelocity;
@@ -71,6 +90,13 @@ public class FlabbyBird extends ApplicationAdapter {
 						Gdx.graphics.getHeight()/2+ gap/2 + tubeOffset[i]);
 				batch.draw(bottomTube,tubeX[i],
 						Gdx.graphics.getHeight()/2-gap/2-bottomTube.getHeight() + tubeOffset[i]);
+				topTubeRectangles[i] =
+						new Rectangle(tubeX[i],
+								Gdx.graphics.getHeight()/2+ gap/2 + tubeOffset[i],
+								topTube.getWidth(),topTube.getHeight());
+				buttonTubeRectangles[i] = new Rectangle(tubeX[i],
+						Gdx.graphics.getHeight()/2-gap/2-bottomTube.getHeight() + tubeOffset[i],
+						bottomTube.getWidth(),bottomTube.getHeight());
 			}
 			if(birdY > 0 || velocity < 0 ){
 				velocity = velocity + gravity;
@@ -88,8 +114,26 @@ public class FlabbyBird extends ApplicationAdapter {
 		else{
 			flapState = 0;
 		}
+
 		batch.draw(birds[flapState],Gdx.graphics.getWidth()/2 - birds[flapState].getWidth()/2, birdY);
 		batch.end();
+		birdCircle.set(Gdx.graphics.getWidth()/2,birdY + birds[flapState].getHeight()/2,
+				birds[flapState].getWidth()/2);
+		//shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
+		//shapeRenderer.setColor(Color.RED);
+		//shapeRenderer.circle(birdCircle.x,birdCircle.y,birdCircle.radius);
+		for (int i = 0; i < numberOfTubes; i++){
+			//shapeRenderer.rect(tubeX[i],
+			//		Gdx.graphics.getHeight()/2+ gap/2 + tubeOffset[i],
+			//		topTube.getWidth(),topTube.getHeight());
+			//shapeRenderer.rect(tubeX[i],
+			//		Gdx.graphics.getHeight()/2-gap/2-bottomTube.getHeight() + tubeOffset[i],
+			//		bottomTube.getWidth(),bottomTube.getHeight());
+			if(Intersector.overlaps(birdCircle,topTubeRectangles[i]) || Intersector.overlaps(birdCircle,buttonTubeRectangles[i])){
+				Gdx.app.log("Collosion","Yes!");
+			}
+		}
+		//shapeRenderer.end();
 	}
 	
 	@Override
